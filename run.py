6a6afd3653c7e5bdcb0463901909b6b3d3b636dd
Gabriel Lopez <gla280900@gmail.com>
2022-07-23 11:58:57 -0500

    test

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from src import app
+# from src import app
 
-# app = create_app()
+# # app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
